d89e428713ee1a85339f0bb4f5052a16bab55881
suceleste <47870191+suceleste@users.noreply.github.com>
2025-11-04 16:28:03 +0100

    Add WinBoost V1

---
+++ WinBoost.bat
@@ -0,0 +1,17 @@
+@echo off
+title WinBoost v1.0
+>nul 2>&1 chcp 65001
+
+goto main
+
+:main
+    cls
+
+    echo.  __      __.__      __________                       __   
+    echo. /  \    /  \__| ____\______   \ ____   ____  _______/  |_ 
+    echo. \   \/\/   /  |/    \|    |  _//  _ \ /  _ \/  ___/\   __\
+    echo.  \        /|  |   |  \    |   (  <_> |  <_> )___ \  |  |  
+    echo.   \__/\  / |__|___|  /______  /\____/ \____/____  > |__|  
+    echo.        \/          \/       \/                  \/        
+
+    set /p Choix = "Quelle est votre choix : "
